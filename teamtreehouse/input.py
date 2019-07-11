05fca83740004c5034d3c0ee7012a9aa137211f0
volhatt <volha.qa@gmail.com>
2019-07-11 15:57:52 -0700

    input output files

--- teamtreehouse/input.py
+++ teamtreehouse/input.py
@@ -1,3 +1,5 @@
+import sys
+
 def rememberer(thing):
     # open file
     # 'WITH' keyword - instead of file.close
@@ -7,6 +9,19 @@ def rememberer(thing):
         # close file
         file.close()
 
+def show():
+    # open file
+    with open('E:\\code\\test\\database.txt', 'r') as file:
+        for line in file:
+            print(line)
+    # print out each line in file
+    # close file
+    pass
+
 
 if __name__ == '__main__':
-    rememberer(input("What should I remember? "))
+    if sys.argv[1].lower() == '--list':
+        show()
+    else:
+        rememberer(' '.join(sys.argv[1:]))
+        #rememberer(input("What should I remember? "))
