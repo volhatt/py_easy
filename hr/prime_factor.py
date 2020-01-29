ace0ee4a21ce5526ee0f2a620e7c98bdb6085c31
volhatt <volha.qa@gmail.com>
2020-01-28 16:23:26 -0800

    prime factorization of number added.

---
+++ hr/prime_factor.py
@@ -0,0 +1,39 @@
+# Python script to found :
+# prime factors of the number
+# divisors of the number
+
+import math
+
+# function that prints all prime factors of a given number
+def primeFactors(n):
+    """
+        Function return array with all prime factors of a given number.
+    """
+    # empty array to store prime factors
+    prime_factors = []
+    # 1. while n divisible by 2 store 2 as prime factor and divide n by 2
+    while n % 2 == 0:
+        prime_factors.append(2)
+        n = n / 2
+        # print(n)
+    # now n should be odd as it is not dividable by 2 
+    # now check numbers from 3 to square root of a given number n with step 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        # while i divides n , store i and divide n by i
+        while n % i == 0:
+            prime_factors.append(i)
+            n = n / i
+            # print(n)
+    # check if last n is a prime number and add it to array
+    if n > 2:
+        prime_factors.append(int(n))
+    
+
+    return prime_factors
+
+print(primeFactors(100))
+# output
+# [2, 2, 5, 5]
+print(primeFactors(6542312))
+# output
+# [2, 2, 2, 7, 116827]
